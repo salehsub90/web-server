fa064fbabf40c443e07d1dd71984ce069ea5b56d
Saleh Suboh <salehsuboh@gmail.com>
2017-02-16 15:58:14 -0500

    Update language

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About US');
+	res.send('About US !');
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -27,58 +27,3 @@ app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function () {
 	console.log('Express server started on port # '+ PORT + '!');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
